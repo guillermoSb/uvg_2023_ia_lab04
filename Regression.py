c62b2e39540265a82d7d35f0cd4bdadc59712fb7
Guillermo Santos <guillermo.santosba@gmail.com>
2023-03-27 08:33:15 -0600

    Implemented logistic regression

---
+++ Regression.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# 0. Create logistic function
+def sigmoid(theta, X):
+		return 1/(1 + np.exp(X@-theta))
+
+
+# 1. Create loss function
+def loss(X, y, theta):
+		N = len(y)
+		return 1/(2*N) * np.sum((sigmoid(theta, X) - y)**2)
+
+# 2. Create gradient function
+def gradient(X, y, theta):
+		N = len(y)
+		return 1/N * X.T@((sigmoid(theta, X) - y) * sigmoid(theta, X) * (1 - sigmoid(theta, X)))
+
+# 3. Create linear regression function
+def linear_regression(X, y, theta, learning_rate, threshold = 0.05):
+		while True:
+			theta = theta - learning_rate * gradient(X, y, theta)
+			if np.sqrt(np.sum(gradient(X, y, theta)**2)) < threshold:
+				return theta
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import Regression as reg
 
 # Load CSV File
-# Columns to use sysBP, glucose, TenYearCHD
-data = np.genfromtxt('framingham.csv', delimiter=',',usecols=(10,14,15), skip_header=True)
+data = np.genfromtxt('framingham.csv', delimiter=',',usecols=(0,5,1,4,7,8,9,10,11,14,15), skip_header=True)
 
 
 # Cleaning the data
@@ -11,12 +11,53 @@ mean = np.nanmean(data, axis=0)	# Calculate mean of each column
 nan_indices = np.where(np.isnan(data))
 data[nan_indices] = np.take(mean, nan_indices[1])	# Replace nan with mean
 
+# Normalization except the last column
+for i in range(data.shape[1] - 1):
+	data[:, i] = (data[:, i] - np.mean(data[:, i])) / np.std(data[:, i])
 
-# Plot sysBP vs glucose with TenYearCHD as color
-plt.scatter(data[:,0], data[:,1], c=data[:,2], cmap='bwr')
-plt.xlabel('sysBP')
-plt.ylabel('glucose')
-plt.show()
 
-# how cmap works
-# https://matplotlib.org/3.1.0/tutorials/colors/colormaps.html
+# Set the seed
+np.random.seed(0)
+# Splitting the data 40% training, 40% cross validation, 20% test
+np.random.shuffle(data)
+train_data = data[:int(0.4*len(data))]
+cv_data = data[int(0.4*len(data)):int(0.8*len(data))]
+test_data = data[int(0.8*len(data)):]
+print("Training data: ", train_data.shape)
+print("Cross Validation data: ", cv_data.shape)
+print("Test data: ", test_data.shape)
+
+# Training the model - y is the last column
+X = train_data[:, :-1]
+y = train_data[:, -1]
+# Reshape y to be a column vector
+y = y.reshape(y.shape[0], 1)
+print("Features shape: ", X.shape)
+print("Labels shape: ", y.shape)
+# theta with zeros
+theta = np.zeros((X.shape[1], 1))
+theta = reg.linear_regression(X, y, theta, 0.05, 0.002)
+print("Theta shape: ", theta.shape)
+
+# Testing the model
+X = test_data[:, :-1]
+y = test_data[:, -1]
+y = y.reshape(y.shape[0], 1)
+
+# Predicting the labels
+y_pred = reg.sigmoid(theta, X)
+y_pred = np.where(y_pred > 0.5, 1, 0)
+print(y_pred)
+
+# Get the accuracy of the model
+accuracy = np.sum(y_pred == y) / len(y)
+print("Accuracy: ", accuracy)
+# Get the presicion of the model
+precision = np.sum(y_pred[y == 1] == 1) / np.sum(y_pred == 1)
+print("Precision: ", precision)
+
+
+
+
+
+# https://www.kaggle.com/code/adithyabshetty100/coronary-heart-disease-prediction/notebook#Feature-Selection
