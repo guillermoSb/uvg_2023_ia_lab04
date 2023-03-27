3b03a4dd5713801c3f6ff119dabf9c25ff18e6d2
Guillermo Santos <guillermo.santosba@gmail.com>
2023-03-27 08:37:30 -0600

    Implemented Logistic Regression

--- Regression.py
+++ Regression.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 # 0. Create logistic function
 def sigmoid(theta, X):
 		return 1/(1 + np.exp(X@-theta))
@@ -17,7 +17,17 @@ def gradient(X, y, theta):
 
 # 3. Create linear regression function
 def linear_regression(X, y, theta, learning_rate, threshold = 0.05):
+		# Store the squad error per example
+		errors = []
 		while True:
 			theta = theta - learning_rate * gradient(X, y, theta)
+			# Append the squad error per example
+			errors.append(loss(X, y, theta))
 			if np.sqrt(np.sum(gradient(X, y, theta)**2)) < threshold:
+				# Plot the errors
+				plt.plot(errors)
+				plt.xlabel("Iterations")
+				plt.ylabel("Loss")
+				plt.show()
+
 				return theta
